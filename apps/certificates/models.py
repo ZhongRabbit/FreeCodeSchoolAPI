b0a5a3383a9f55da6c0938106904290b5e2d3bad
Zhong Gao <bayansuren@gmail.com>
2021-07-17 19:53:58 -0400

    add certificates app

--- apps/certificates/models.py
+++ apps/certificates/models.py
@@ -1,7 +1,7 @@
 from django.db import models
-from apps.utils.models import Timestamps
+from apps.utils.models import AbstractTableMeta
 
-class Certificate(Timestamps,
+class Certificate(AbstractTableMeta,
                   models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField()
--- apps/utils/models.py
+++ apps/utils/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class Timestamps(models.Model):
+class AbstractTableMeta(models.Model):
     created_at = models.DateField(auto_now_add=True)
     updated_at = models.DateField(auto_now=True)
 
